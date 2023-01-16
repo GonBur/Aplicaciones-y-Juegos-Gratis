7ef515102a97ff0ec69f340d68bfe6e5eb491ffa
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-01-16 13:11:35 -0500

    se agrega el proyecto

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/* "use script"
+mensajeAlerta =document.getElementById("body").innerHTML
+
+    mensajeAlerta = alert("Esta pagina web nos permite la descarga de algunos videojuegos y aplicaciones crackeadas y usarlos de manera libre y segura") */
